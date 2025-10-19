89440d00e2eb837c88f491f30dbe65c7ec29ec9d
yuzhenlong02 <yuzhenlong02@meituan.com>
2025-10-19 17:12:26 +0800

    1019: step1单一数据集训练通过，lora微调正常;step2增加双路输入训练通过；step3增加双路评估，修复评估报错，三分类映射二分类

--- run_eval_dual_binary.sh
+++ run_eval_dual_binary.sh
@@ -3,7 +3,7 @@
 # 评估双路输入模型的脚本（三分类 → 二分类）
 
 # ✅ 配置参数
-MODEL_PATH="/root/autodl-fs/output/classi_dual_20251019_131545"  # 训练输出的 checkpoint 目录
+MODEL_PATH="/root/autodl-fs/output/classi_dual_20251019_131545/checkpoint-3000"  # 训练输出的 checkpoint 目录
 BASE_MODEL_PATH="/root/autodl-tmp/CultureMoE/Culture_Alignment/Meta-Llama-3.1-8B-Instruct"  # 原始 LLaMA 模型路径
 TEST_FILE="/root/autodl-fs/CulturalBench_Hard_merge.json"  # 二分类测试集
 OUTPUT_FILE="/root/autodl-fs/eval_results_binary_$(date +%Y%m%d_%H%M%S).json"
