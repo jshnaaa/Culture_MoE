a8562bacf80bfd0102b0c8ffb8e7fd3875134ce9
yuzhenlong02 <yuzhenlong02@meituan.com>
2025-11-14 23:40:27 +0800

    1112：run_ft_culturemoe_gen.sh，run_ft_lora_only_gen.sh，run_eval_lora_only_from_components.sh，run_eval_culturemoe_from_components.sh; 1

--- run_ft_culturemoe_gen.sh
+++ run_ft_culturemoe_gen.sh
@@ -99,7 +99,7 @@ case $DATA_ID in
         TRAIN_FILE="/root/autodl-fs/cultureLLM_merge_gen_small.json"
         DATASET_TAG="cultureLLM"
         if [ "$BACKBONE" = "qwen" ]; then
-            LORA_WEIGHTS_PATH="/root/autodl-tmp/CultureMoE/Culture_Alignment/ft/ft_lora_only_gen_cultureLLM_qwen_20251112_1551/best_lora"
+            LORA_WEIGHTS_PATH="/root/autodl-tmp/CultureMoE/Culture_Alignment/ft/ft_lora_only_gen_cultureLLM_qwen_20251114_1300/best_lora"
         else
             LORA_WEIGHTS_PATH="/root/autodl-tmp/CultureMoE/Culture_Alignment/ft/ft_lora_only_gen_cultureLLM_llama_20251112_1551/best_lora"
         fi
