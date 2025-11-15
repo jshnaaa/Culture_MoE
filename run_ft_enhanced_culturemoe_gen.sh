52a36fb16e0aa3f6b95f90938885c97abf17661e
yuzhenlong02 <yuzhenlong02@meituan.com>
2025-11-16 00:08:40 +0800

    1112：run_ft_culturemoe_gen.sh，run_ft_lora_only_gen.sh，run_eval_lora_only_from_components.sh，run_eval_culturemoe_from_components.sh; 8

--- run_ft_enhanced_culturemoe_gen.sh
+++ run_ft_enhanced_culturemoe_gen.sh
@@ -142,8 +142,8 @@ OUTPUT_DIR="/root/autodl-fs/data/ft/ft_enhanced_moe_gen_${DATASET_TAG}_${BACKBON
 
 # 设置 GPU
 if [ "$NUM_GPUS" = "1" ]; then
-    export CUDA_VISIBLE_DEVICES=0
-    GPU_INFO="Single GPU (GPU 0)"
+#    export CUDA_VISIBLE_DEVICES=0
+    GPU_INFO="Single GPU (GPU)"
 elif [ "$NUM_GPUS" = "2" ]; then
     export CUDA_VISIBLE_DEVICES=0,1
     GPU_INFO="Dual GPUs (GPU 0,1)"
