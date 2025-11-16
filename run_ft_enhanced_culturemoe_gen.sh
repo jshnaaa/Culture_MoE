dfde3aa0286e84a6b589e992b6e8a625e6263eac
yuzhenlong02 <yuzhenlong02@meituan.com>
2025-11-16 10:06:01 +0800

    1112：run_ft_enhanced_culturemoe_gen.sh，run_eval_lora_only_from_components.sh; 0

--- run_ft_enhanced_culturemoe_gen.sh
+++ run_ft_enhanced_culturemoe_gen.sh
@@ -275,7 +275,7 @@ python ft_enhanced_culturemoe_gen.py \
     --experts_hidden_dim 4096 \
     --moe_lora_rank 32 \
     --dropout 0.05 \
-    --batch_size $(if [ "$BACKBONE" = "llama" ]; then echo "3"; else echo "8"; fi) \
+    --batch_size $(if [ "$BACKBONE" = "llama" ]; then echo "3"; else echo "6"; fi) \
     --eval_batch_size $(if [ "$BACKBONE" = "llama" ]; then echo "2"; else echo "4"; fi) \
     --learning_rate 2e-4 \
     --moe_lr_multiplier 1.0 \
