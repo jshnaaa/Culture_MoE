e06d0ac5aa267f1bbfdb2e6dee1b0c807a713b1b
yuzhenlong02 <yuzhenlong02@meituan.com>
2025-12-05 12:26:51 +0800

    1124：bash run_joint_lora_moe_training.sh; 把文件都改了prompt格式；加文化损失；normad；1

--- run_eval_joint_lora_moe_from_base.sh
+++ run_eval_joint_lora_moe_from_base.sh
@@ -65,7 +65,7 @@ if [ "$BACKBONE" = "llama" ]; then
 else
     BASE_MODEL_PATH="/root/autodl-tmp/CultureMoE/Culture_Alignment/Meta-Qwen-2.5-7B-Instruct"
     MODEL_NAME="Qwen 2.5-7B-Instruct"
-    JOINT_MODEL_PATH="/autodl-fs/data/joint_lora_moe/qwen_cultureLLM_sharedfalse_gatefalse_20251204_152323"
+    JOINT_MODEL_PATH="/autodl-fs/data/joint_lora_moe/qwen_cultureLLM_sharedfalse_gatefalse_20251204_152323/best_joint_model"
 fi
 
 # ✅ 根据 DATA_ID 设置测试数据集
