78bc0d3c177f602925291b33eea4a7d369ffa4eb
yuzhenlong02 <yuzhenlong02@meituan.com>
2025-12-31 14:50:47 +0800

    1230:run_joint_lora_moe_training.sh；run_eval_joint_culturemoe.sh;加USE_MASK；推理消融；推理成功；运行batch推理；3

--- run_batch.sh
+++ run_batch.sh
@@ -3,16 +3,19 @@ bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalB
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 0 true false
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 0 false
 
+bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 5 true true true true
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 5 true true true false
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 5 true true false
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 5 true false
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 5 false
 
+bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 4 true true true true
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 4 true true true false
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 4 true true false
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 4 true false
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 4 false
 
+bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 3 true true true true
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 3 true true true false
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 3 true true false
 bash run_eval_joint_culturemoe.sh /autodl-fs/data/joint_lora_moe/llama_CulturalBench_sharedtrue_gatetrue_20251230_212244 llama 3 true false
