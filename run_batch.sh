cdbe7516cdc612c4fbf8ff9781bb6ededadde5b1
yuzhenlong02 <yuzhenlong02@meituan.com>
2026-01-03 23:20:35 +0800

    0103:run_joint_lora_moe_training.sh；run_eval_joint_culturemoe.sh;加USE_MASK；推理mask配置修复；训练加24数据集；推理消融参数功能修复；blend分组；8

--- run_batch.sh
+++ run_batch.sh
@@ -1,6 +1,6 @@
 echo "blend llama (with country field for grouping)"
 bash run_ft_base.sh llama 161
-echo "blend rp qwen (with country field for grouping)"
+echo "blend qwen (with country field for grouping)"
 bash run_ft_base.sh qwen 161
 echo "blend rp llama (with country field for grouping)"
 bash run_ft_base.sh llama 162
